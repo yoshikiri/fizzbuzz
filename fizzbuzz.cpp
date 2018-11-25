295f4c43f53421e1d70e80d143236aa12a31f6e8
Momori <elise.nicopy@gmail.com>
2018-11-25 23:53:13 +0900

    Add fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+  const int N = 20;
+
+  for(int i=0;i<N;i++){
+    if(i % 15 == 0) std::cout << "FizzBuzz" << '\n';
+    else if(i % 5 == 0) std::cout << "Buzz" << '\n';
+    else if(i % 3 == 0) std::cout << "Fizz" << '\n';
+    else std::cout << i << '\n';
+  }
+
+  return 0;
+}

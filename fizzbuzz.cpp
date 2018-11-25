082418bf8d251df8be8700fe0ef7f098c1459b67
Momori <elise.nicopy@gmail.com>
2018-11-26 00:03:12 +0900

    Add function for judge fizzbuzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
+void fizzbuzz(int n){
+  if(n % 15 == 0) std::cout << "FizzBuzz" << '\n';
+  else if(n % 7 == 0) std::cout << "GitHub" << '\n';
+  else if(n % 5 == 0) std::cout << "Buzz" << '\n';
+  else if(n % 3 == 0) std::cout << "Fizz" << '\n';
+  else std::cout << n << '\n';
+}
+
 int main(){
   const int N = 20;
 
   for(int i=0;i<N;i++){
-    if(i % 15 == 0) std::cout << "FizzBuzz" << '\n';
-    else if(i % 7 == 0) std::cout << "GitHub" << '\n';
-    else if(i % 5 == 0) std::cout << "Buzz" << '\n';
-    else if(i % 3 == 0) std::cout << "Fizz" << '\n';
-    else std::cout << i << '\n';
+    fizzbuzz(i);
   }
 
   return 0;

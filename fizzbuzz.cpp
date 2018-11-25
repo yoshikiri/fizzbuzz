3d1112be7cd94815fbb811321a25eaab7cd5e3e9
Momori <elise.nicopy@gmail.com>
2018-11-25 23:57:04 +0900

    Add output GitHub

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -5,6 +5,7 @@ int main(){
 
   for(int i=0;i<N;i++){
     if(i % 15 == 0) std::cout << "FizzBuzz" << '\n';
+    else if(i % 7 == 0) std::cout << "GitHub" << '\n';
     else if(i % 5 == 0) std::cout << "Buzz" << '\n';
     else if(i % 3 == 0) std::cout << "Fizz" << '\n';
     else std::cout << i << '\n';
